f7c079ad3b71e1e3ec6a4b4e09400f7e270ad59e
Trevor Leake <contact@trevorleake.com>
2019-05-29 12:20:36 -0700

    Implemented RPN logic for the calculator & modified the index page.

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,56 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
 
-#[get("/?<query_string>")]
-fn string_adder(query_string: String) -> String {
-    let split_string: Vec<&str> = query_string.split("+").collect();
 
-    let mut response_string = String::from("The sum of ");
+fn rpn(s: String) -> String {
 
-    for x in 0..split_string.len() {
-        response_string.push_str(split_string[x]);
+    let mut command_stack: Vec<&str> = s.split_whitespace().rev().collect();
+    let mut calculator_stack: Vec<f64> = vec![];
 
-        if x != split_string.len()-1 {
-            response_string.push_str(", ");
+    while command_stack.len() > 0 {
+        // Take from command string and place on the stack
+        let input: &str = command_stack.pop().unwrap();
+
+        // If the input is a number
+        if input.parse::<f64>().is_ok() {
+            // Add to calculator stack
+            calculator_stack.push(input.parse::<f64>().unwrap());
+        } else {
+            // Do what the operator says
+            let b = calculator_stack.pop().unwrap();
+            let a = calculator_stack.pop().unwrap();
+
+            if input == "+" {
+                calculator_stack.push(a+b);
+            }
+
+            if input == "*" {
+                calculator_stack.push(a*b);
+            }
+
+            if input == "-" {
+                calculator_stack.push(a-b);
+            }
+
+            if input == "/" {
+                calculator_stack.push(a/b);
+            }
         }
     }
 
-    let sum: i32 = adder(query_string);
-    response_string.push_str(&format!(" is {}.", &sum.to_string()));
-
-    return response_string;
+    // Computation is done, the answer should be left
+    return calculator_stack[0].to_string();
 }
 
-fn adder(query_string: String) -> i32 {
-    let split_string = query_string.split("+");
 
-    let mut sum: i32 = 0;
-    for x in split_string {
-        let s: i32 = x.parse().unwrap();
-        sum += s;
-    }
-
-    return sum;
+#[get("/?<query_string>")]
+fn string_rpn(query_string: String) -> String {
+    return rpn(query_string);
 }
 
 fn main() {
     rocket::ignite()
         .mount("/", rocket_contrib::serve::StaticFiles::from("static"))
-        .mount("/adder", routes![string_adder])
+        .mount("/rpn", routes![string_rpn])
         .launch();
 }
